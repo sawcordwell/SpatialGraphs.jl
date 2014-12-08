b3afea4ff76182a4f2546e3dc6a20f9cc5916a49
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2014-12-08 15:32:19 +1300

    [types] Minimal working package
    Add a simple geometry and vertex type that could constitute a minimal
    amount of functionality if all you needed was to store vertexes in a
    graph

--- src/SpatialGraphs.jl
+++ src/SpatialGraphs.jl
@@ -1,3 +1,44 @@
 module SpatialGraphs
 
+using Graphs
+
+import Graphs: vertex_index
+
+export # Types
+       Geometry,
+       Point,
+       SimpleSpatialVertex,
+       SpatialVertex,
+       # Graphs.jl interfaces
+       vertex_index,
+       # Spatial interfaces
+       geometry,
+       # Visualisation
+       plot
+
+
+abstract SpatialVertex
+abstract Geometry{T<:Real}
+
+vertex_index(v::SpatialVertex) = v.index
+geometry(v::SpatialVertex) = v.geom
+
+immutable Point{T} <: Geometry{T}
+  x::T
+  y::T
+end
+
+(+)(a::Point, b::Point) = Point(a.x + b.x, a.y + b.y)
+(-)(a::Point, b::Point) = Point(a.x - b.x, a.y - b.y)
+(*)(p::Point, s::Real) = Point(p.x*s, p.y*s)
+(*)(s::Real, p::Point) = p*s
+(==)(a::Point, b::Point) = ==(a.x, b.x) && ==(a.y, b.y)
+
+type SimpleSpatialVertex{T} <: SpatialVertex
+  index::Int
+  geom::Geometry{T}
+end
+SimpleSpatialVertex{T}(id::Int, x::T, y::T) =
+  SimpleSpatialVertex(id, Point(x, y))
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,13 @@
 using SpatialGraphs
 using Base.Test
+
+@test Point{Float64} <: Geometry{Float64}
+
+@test Point(1, 2) + Point(2, 1) == Point(3, 3)
+@test Point(1.0, 2.0) + Point(2, 1) == Point(3.0, 3.0)
+@test Point(1, 2) * 2 == Point(2, 4)
+@test Point(1, 2) * 2.0 == Point(2.0, 4.0)
+@test 2 * Point(1, 2) == Point(2.0, 4.0)
+
+SimpleSpatialVertex(1, Point(1.2, 3.5))
+SimpleSpatialVertex(1, 1.2, 3.5)
